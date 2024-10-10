edb72919e044d98f05b6b506ea0f9e5bf942a94d
zurikiser <softwatercreatives@gmail.com>
2024-10-10 14:38:00 -0400

    git test

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.add.url.rule('/<name>', 'name_page', test_func)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Zuri'
+    return 'Hello Zuri Barnes'
 
 # main driver function
 if __name__ == '__main__':
